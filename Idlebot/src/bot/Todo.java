c7cdaee27ee0c4d1c251aa15786c8902f565e757
imnothere199@gmail.com <imnothere199@gmail.com@45aff4f1-60a0-9de5-5af6-f5e3312fac0d>
2012-01-01 19:18:22 +0000

    Modified todo for array fixing.
    git-svn-id: https://idlemaster.googlecode.com/svn/trunk@29 45aff4f1-60a0-9de5-5af6-f5e3312fac0d

--- Idlebot/src/bot/Todo.java
+++ Idlebot/src/bot/Todo.java
@@ -4,6 +4,7 @@ public class Todo {
 }
 
 /**
+ * add a -1 to all of the array indexes in the events. 
  * add offhand weapons to shields
  * temporarily sell items to other players (they can try to equip it then) (charisma potion?)
  * change password system to use md5 (for idlebotrpg)
