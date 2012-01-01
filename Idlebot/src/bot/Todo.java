59ea5b5aa1f3213f4803de0b0fbab032d6156fc7
imnothere199@gmail.com <imnothere199@gmail.com@45aff4f1-60a0-9de5-5af6-f5e3312fac0d>
2012-01-01 23:59:48 +0000

    another todo
    git-svn-id: https://idlemaster.googlecode.com/svn/trunk@32 45aff4f1-60a0-9de5-5af6-f5e3312fac0d

--- Idlebot/src/bot/Todo.java
+++ Idlebot/src/bot/Todo.java
@@ -5,6 +5,7 @@ public class Todo {
 
 /**
  * add a -1 to all of the array indexes in the events. 
+  * fix battles showing a space between team names: Team Sei {name [], name[] }
  * add offhand weapons to shields
  * temporarily sell items to other players (they can try to equip it then) (charisma potion?)
  * change password system to use md5 (for idlebotrpg)
