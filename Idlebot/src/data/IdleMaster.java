74b5f23831afafdfcbc26cdf0643d1fdd5a30448
imnothere199 <imnothere199@45aff4f1-60a0-9de5-5af6-f5e3312fac0d>
2012-08-02 13:27:59 +0000

    Idlemasters items are now idle class
    git-svn-id: https://idlemaster.googlecode.com/svn/trunk@196 45aff4f1-60a0-9de5-5af6-f5e3312fac0d

--- Idlebot/src/data/IdleMaster.java
+++ Idlebot/src/data/IdleMaster.java
@@ -13,16 +13,16 @@ public class IdleMaster extends Player {
 	
 	@Override
 	protected void generateNewEquipment() {
-		equipment.put(Slot.Body, new Item("Avaracious Armor of the Idling Gods", 5000, Type.Physical, Item.ItemClass.Special));
-		equipment.put(Slot.Feet, new Item("Fastidious Feet of the Idling Gods", 5000, Type.Physical, Item.ItemClass.Special));
-		equipment.put(Slot.Finger, new Item("Rebellious Ring of the Idling Gods", 5000, Type.Magical, Item.ItemClass.Special));
-		equipment.put(Slot.Hands, new Item("Glowing Gauntlets of the Idling Gods", 5000, Type.Physical, Item.ItemClass.Special));
-		equipment.put(Slot.Head, new Item("Feathered Fez of the Idling Gods",5000, Type.Physical, Item.ItemClass.Special));
-		equipment.put(Slot.Legs, new Item("Glowing Greaves of the Idling Gods", 5000, Type.Physical, Item.ItemClass.Special));
-		equipment.put(Slot.Neck, new Item("Transcendent Torc of the Idling Gods", 5000, Type.Magical, Item.ItemClass.Special));
-		equipment.put(Slot.Shield, new Item("Supersonic Shield of the Idling Gods", 5000, Type.Physical, Item.ItemClass.Special));
-		equipment.put(Slot.Weapon, new Item("Wingsaber, the Whirling Blade", 5000, Type.Emotional, Item.ItemClass.Special));
-		equipment.put(Slot.Charm, new Item("Cosmic Charm of the Idling Gods", 5000, Type.Spiritual, Item.ItemClass.Special));
+		equipment.put(Slot.Body, new Item("Avaracious Armor of the Idling Gods", 5000, Type.Physical, Item.ItemClass.Idle));
+		equipment.put(Slot.Feet, new Item("Fastidious Feet of the Idling Gods", 5000, Type.Physical, Item.ItemClass.Idle));
+		equipment.put(Slot.Finger, new Item("Rebellious Ring of the Idling Gods", 5000, Type.Magical, Item.ItemClass.Idle));
+		equipment.put(Slot.Hands, new Item("Glowing Gauntlets of the Idling Gods", 5000, Type.Physical, Item.ItemClass.Idle));
+		equipment.put(Slot.Head, new Item("Feathered Fez of the Idling Gods",5000, Type.Physical, Item.ItemClass.Idle));
+		equipment.put(Slot.Legs, new Item("Glowing Greaves of the Idling Gods", 5000, Type.Physical, Item.ItemClass.Idle));
+		equipment.put(Slot.Neck, new Item("Transcendent Torc of the Idling Gods", 5000, Type.Magical, Item.ItemClass.Idle));
+		equipment.put(Slot.Shield, new Item("Supersonic Shield of the Idling Gods", 5000, Type.Physical, Item.ItemClass.Idle));
+		equipment.put(Slot.Weapon, new Item("Wingsaber, the Whirling Blade", 5000, Type.Emotional, Item.ItemClass.Idle));
+		equipment.put(Slot.Charm, new Item("Cosmic Charm of the Idling Gods", 5000, Type.Spiritual, Item.ItemClass.Idle));
 	}
 
 	@Override
